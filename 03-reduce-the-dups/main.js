c882883e405f3d62797921df47f9e7bd9f752630
Michael Wildnauer <michael.wildnauer4481@gmail.com>
2020-05-11 15:39:10 -0400

    reduce the dups finished

--- 03-reduce-the-dups/main.js
+++ 03-reduce-the-dups/main.js
@@ -10,3 +10,24 @@
 
 const redundantArray = [1, 1, 2, 3, 3, 4]
 // YOUR CODE HERE
+const goButton = document.querySelector("#go")
+const initial = document.querySelector("#initial")
+const ammended = document.querySelector("#ammended")
+let index = 0
+
+initial.innerText = redundantArray
+
+goButton.addEventListener('click', function() {
+  while(index < redundantArray.length) {
+
+    let current = redundantArray[index]
+    
+    if(current == redundantArray[index + 1]) {
+      redundantArray.splice(index, 1)
+    }
+    else {
+      index++
+    }
+  }
+  ammended.innerText = redundantArray
+})
